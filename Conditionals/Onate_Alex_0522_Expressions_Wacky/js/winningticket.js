5efeb84f4aa91b77f57b0e3989391bbd46db6f6e
Alex Onate <alexonate@gmail.com>
2014-05-22 23:00:14 -0400

    you won!

--- Conditionals/Onate_Alex_0522_Expressions_Wacky/js/winningticket.js
+++ Conditionals/Onate_Alex_0522_Expressions_Wacky/js/winningticket.js
@@ -4,3 +4,12 @@ var winningTicket = 500;
 var notWinner = 0;
 var winner = true;
 
+//if the ticket is less than the winning ticket and if the winner is true
+
+if( notWinner < winningTicket || winner === true){
+    //if true the condition will perform
+    console.log("you won!");
+}else{
+   //if false condition will perform
+    console.log("you lost!");
+}
