c07563a7ffe600441a9b4d79cdff092025c8a959
Alex Onate <alexonate@gmail.com>
2014-05-22 22:31:58 -0400

    console print out

--- Conditionals/Onate_Alex_0522_Expressions_Personal/js/passingtheclass.js
+++ Conditionals/Onate_Alex_0522_Expressions_Personal/js/passingtheclass.js
@@ -7,6 +7,7 @@ var extraCredit = 5;
 //if the student has the minimum grade, print to the console "you pass :)"
 //if the student has 70 or over
 
-if(studentfailingGrade + extraCredit > studentpassingGrade)
-
-//if true the condition will perform
+if(studentfailingGrade + extraCredit > studentpassingGrade){
+    //if true the condition will perform
+    console.log(""you pass :)"")
+}
