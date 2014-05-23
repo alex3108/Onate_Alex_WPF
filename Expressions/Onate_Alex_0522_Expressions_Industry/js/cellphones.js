f5300edb63b549dd49e7d311f1fdb29719629499
Alex Onate <alexonate@gmail.com>
2014-05-22 22:22:03 -0400

    conditional logic

---
+++ Expressions/Onate_Alex_0522_Expressions_Industry/js/cellphones.js
@@ -0,0 +1,19 @@
+////Expressions with Array
+
+
+//cell phones brands & prices
+
+var cellPhones = [500 + 300 + 400];
+ 
+
+//buy 3 phones today
+
+var total = cellPhones[0] + cellPhones[1] + cellPhones[2];
+
+//console.log out the answer
+console.log(total);
+
+alert(total);
+
+
+
