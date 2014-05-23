5f277e29f2ac3f6244e787186985fec45e58fac6
Alex Onate <alexonate@gmail.com>
2014-05-22 23:36:12 -0400

    checking answers

--- Conditionals/Onate_Alex_0522_Expressions_Industry/js/poker.js
+++ Conditionals/Onate_Alex_0522_Expressions_Industry/js/poker.js
@@ -1,8 +1,8 @@
 //Ternary Operators
 
-var player = 5;
+var player = 10;
 var pokerGame;
 
 //if the player has a 8 or better he wins, otherwise he losses the game
-pokerGame = (player < 8) ? "He Wins" : "He Losses";
+pokerGame = (player > 8) ? "He Wins" : "He Losses";
 console.log(pokerGame);
