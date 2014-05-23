c484694b4420ca1c9603ff1d115335648f7c70df
Alex Onate <alexonate@gmail.com>
2014-05-22 23:31:48 -0400

    console.log

--- Conditionals/Onate_Alex_0522_Expressions_Industry/js/poker.js
+++ Conditionals/Onate_Alex_0522_Expressions_Industry/js/poker.js
@@ -1,4 +1,8 @@
 //Ternary Operators
 
-var player = fullhouse;
-var poker;
+var player = 10;
+var pokerGame;
+
+//if the player has a 10 or better he wins, otherwise he losses the game
+pokerGame = (player > 5) ? "He Wins" : "He Losses";
+console.log(pokerGame);
--- Conditionals/Onate_Alex_0522_Expressions_Wacky/js/poker.js
+++
@@ -1,9 +0,0 @@
-//Ternary Operators
-
-//about poker
-var player = 8;
-var pokergame;
-
-//if the player has a 10 or better he wins, otherwise he losses the game
-pokergame = (player > 5 ) ? "He Wins" : "He Losses";
-console.log(pokergame);
