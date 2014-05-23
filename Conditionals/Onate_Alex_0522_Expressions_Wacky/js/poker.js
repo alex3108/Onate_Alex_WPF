5e6cde11bf629cd8ef32472f1c404be859d762e0
Alex Onate <alexonate@gmail.com>
2014-05-22 23:17:26 -0400

    player > 5

--- Conditionals/Onate_Alex_0522_Expressions_Wacky/js/poker.js
+++ Conditionals/Onate_Alex_0522_Expressions_Wacky/js/poker.js
@@ -1,5 +1,8 @@
 //Ternary Operators
 
 //about poker
-var player = fullhouse;
-var poker;
+var player = 8;
+var pokergame;
+
+//if the player has a 10 or better he wins, otherwise he losses the game
+pokergame = (player > 5 ) ? "He Wins" : "He Losses";
