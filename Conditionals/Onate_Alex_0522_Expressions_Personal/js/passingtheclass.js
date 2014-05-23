2e5a1c72c2b8ebce6d4ad083c31692df7075965d
Alex Onate <alexonate@gmail.com>
2014-05-22 22:28:48 -0400

    conditional logic

---
+++ Conditionals/Onate_Alex_0522_Expressions_Personal/js/passingtheclass.js
@@ -0,0 +1,12 @@
+//Conditional Logic
+
+var studentfailingGrade = 69;
+var studentpassingGrade = 70;
+var extraCredit = 5;
+
+//if the student has the minimum grade, print to the console "you pass :)"
+//if the student has 70 or over
+
+if(studentfailingGrade + extraCredit > studentpassingGrade)
+
+//if true the condition will perform
