15e4495d2ce0fdb469e31f5c597ede7e1e7c6cf9
Alex Onate <alexonate@gmail.com>
2014-05-23 23:34:25 -0400

    The game works.

--- Conditionals/Onate_Alex_0522_Expressions_Industry/js/poker.js
+++ Conditionals/Onate_Alex_0522_Expressions_Industry/js/poker.js
@@ -1,8 +1,30 @@
 //Ternary Operators
 
-var player = 5;
-var pokerGame;
+var player = 0;
+var warGame;
 
-//if the player has a 8 or better he wins, otherwise he losses the game
-pokerGame = (player < 8) ? "He Wins" : "He Losses";
-console.log(pokerGame);
+var welcome = ("Welcome to War");
+console.log(welcome);
+alert(welcome);
+
+var playerName = prompt("Enter your name");
+console.log(playerName);
+alert(playerName);
+
+var begin = ("Lets begin");
+console.log(begin);
+alert(begin);
+
+var player = prompt("Enter your card number");
+//If the player has a 10 or better he wins, otherwise he losses the game
+//Since were are not using strings, but numbers instead "J,Q,K,A" will be "11,12,13,14"
+warGame = (player > 10) ? "You Won" : "You Lost";
+
+var thanks = ("Thanks for playing");
+
+//Validating prompts
+
+console.log(warGame);
+alert(warGame);
+console.log(thanks);
+alert(thanks); 
